c3472e4b5beb989ef1eae942ae9eef2fbd8d9610
Gonçalo Costa Santos <goncalomccs@gmail.com>
2019-11-16 17:08:54 +0000

    Adding the 1st iterations of node lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,24 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor(items, length) {
+    this.items = [];
+    this.length = [];
+  }
+  add(item) {
+    this.items.push(item);  //add method will add item to the items array
+    this.length++;    // then we need to make the length bigger so that we can keep adding more items
+  }
+  get(pos) {
+    if( pos > this.item) {
+    throw new Error("OutOfBounds");
+    } else if (pos < this.item){
+      
+    }
+
+  }
+  max() {}
+  min() {}
+  average() {}
+  sum() {}
+}
+
+module.exports = SortedList;
